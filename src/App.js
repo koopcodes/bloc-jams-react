a197cf631f93457e0063ac9e285113fd9c9c0122
koopdev <koop@koop.codes>
2018-05-02 20:52:18 -0700

    Checkpoint 4 - Landing COMPLETE

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Landing from './components/Landing';
 import Library from './components/Library';
 import Album from './components/Album.js';
 
+//comment
 class App extends Component {
     render() {
         return (
