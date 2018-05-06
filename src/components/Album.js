a51089dabe1647d6281b5a4bf6720370da32ad88
koopdev <koop@koop.codes>
2018-05-06 13:47:18 -0700

    Checkpoint 9 - Player Bar: Range Inputs COMPLETE

--- src/components/Album.js
+++ src/components/Album.js
@@ -14,6 +14,8 @@ class Album extends Component {
     this.state = {
       album: album,
 			currentSong: album.songs[0],
+			currentTime: 0,
+      duration: album.songs[0].duration,
       isPlaying: false
     };
 
@@ -36,6 +38,25 @@ class Album extends Component {
      this.setState({ currentSong: song });
 		}
 
+		componentDidMount() {
+			this.eventListeners = {
+       timeupdate: e => {
+         this.setState({ currentTime: this.audioElement.currentTime });
+       },
+       durationchange: e => {
+         this.setState({ duration: this.audioElement.duration });
+       }
+     };
+     	this.audioElement.addEventListener('timeupdate', this.eventListeners.timeupdate);
+     	this.audioElement.addEventListener('durationchange', this.eventListeners.durationchange);
+		}
+
+		componentWillUnmount() {
+     this.audioElement.src = null;
+     this.audioElement.removeEventListener('timeupdate', this.eventListeners.timeupdate);
+     this.audioElement.removeEventListener('durationchange', this.eventListeners.durationchange);
+   }
+
 		handleSongClick(song) {
      const isSameSong = this.state.currentSong === song;
 		 if (this.state.isPlaying && isSameSong) {
@@ -44,7 +65,7 @@ class Album extends Component {
 			 if (!isSameSong) { this.setSong(song); }
        this.play();
      }
-   }
+		}
 
     handlePrevClick() {
 			const currentIndex = this.state.album.songs.findIndex(song => this.state.currentSong === song);
@@ -62,10 +83,15 @@ class Album extends Component {
       this.play();
 		}
 
-
 		formatTime(time) {
     return time ? `${Math.floor(time / 60)}:${Number(time % 60 / 100).toFixed(2).substr(2,3)}` : '-:--'
   	}
+
+		handleTimeChange(e) {
+     const newTime = this.audioElement.duration * e.target.value;
+     this.audioElement.currentTime = newTime;
+     this.setState({ currentTime: newTime });
+		}
 
   render() {
     return (
@@ -95,11 +121,11 @@ class Album extends Component {
 							<tr className={song} key ={index} onClick={() => this.handleSongClick(song)} >
 								<td className="song-actions">
                   <button>
-                  	<span className={this.state.isPlaying ? 'ion-pause' : 'ion-play'}></span>
+                  	<span className={((this.state.isPlaying) && (this.state.currentSong===song)) ? 'ion-pause' : 'ion-play'}></span>
 									  <span className="song-number"> { index+1 }</span>
                   </button>
                 </td>
-								<td className="song-title"><Link to={`${song.audioSrc}`}>{ song.title }</Link></td>
+								<td className="song-title">{ song.title }</td>
 								<td className="song-duration">{ this.formatTime(song.duration) } </td>
 							</tr>
         ))}
@@ -108,9 +134,12 @@ class Album extends Component {
 				<PlayerBar
            isPlaying={this.state.isPlaying}
            currentSong={this.state.currentSong}
+					 currentTime={this.audioElement.currentTime}
+           duration={this.audioElement.duration}
            handleSongClick={() => this.handleSongClick(this.state.currentSong)}
 					 handlePrevClick={() => this.handlePrevClick()}
 					 handleNextClick={() => this.handleNextClick()}
+					 handleTimeChange={(e) => this.handleTimeChange(e)}
          />
       </section>
     );
